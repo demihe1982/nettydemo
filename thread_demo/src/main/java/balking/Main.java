eb74728582c2125bf72bc22ed9e16ca0d254fba0
edayahe <demi.he@ericsson.com>
2018-04-30 19:26:10 +0800

    init

---
+++ thread_demo/src/main/java/balking/Main.java
@@ -0,0 +1,9 @@
+package balking;
+
+public class Main {
+    public static void main(String[] args){
+        Data data = new Data("data.txt","(empty)");
+        new ChangeThread("ChangeThread",data).start();
+        new SaveThread("SaveThread",data).start();
+    }
+}
